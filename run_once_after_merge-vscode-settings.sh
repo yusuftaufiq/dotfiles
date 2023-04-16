8a0f41d034e1e6481254fd1e03ebdb69751f6d16
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-04-16 13:35:11 +0700

    fix(vscode): only merge when code & jq commands available

--- run_once_after_merge-vscode-settings.sh
+++ run_once_after_merge-vscode-settings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if command -v code >/dev/null 2>&1; then
+if command -v code >/dev/null 2>&1 && command -v jq >/dev/null 2>&1; then
   jq -s '.[0] * .[1]' ~/.config/Code/User/settings.json dot_config/Code/User/settings.json > ~/.config/Code/User/settings.json
 fi
