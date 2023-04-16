f061e8cb6038de2c7a0f86245e3cd669df3829dd
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-04-16 13:33:32 +0700

    feat(vscode): add vscode theme configs

---
+++ run_once_before_install-vscode-extensions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if command -v code >/dev/null 2>&1; then
+  code --install-extension arcticicestudio.nord-visual-studio-code
+  code --install-extension monokai.theme-monokai-pro-vscode
+  code --install-extension PKief.material-product-icons
+fi
