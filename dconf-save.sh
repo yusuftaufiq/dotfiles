46300717057ee07c1400256b38606c07d6b2cedb
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-04-16 16:07:39 +0700

    feat(gnome): add shell script to save selected gnome configs

---
+++ dconf-save.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+configs=(
+  "gnome/shell/extensions/aylurs-widgets" 
+  "gnome/shell/extensions/rounded-window-corners"
+  "gnome/shell/extensions/unite"
+  "gnome/shell/extensions/dash-to-dock-pop"
+  "gnome/shell/extensions/pop-shell"
+  "gnome/shell/extensions/pop-cosmic"
+  "gnome/shell/extensions/com/github/hermes83/compiz-windows-effect"
+  "gnome/shell/extensions/ncom/github/hermes83/compiz-alike-magic-lamp-effect"
+  "gnome/shell/extensions/user-theme"
+  "gnome/desktop/interface"
+  "gnome/desktop/wm/keybindings"
+  "gnome/desktop/wm/preferences"
+  "gnome/gedit/preferences/editor"
+  "gnome/mutter"
+  "gnome/nautilus/icon-view"
+  "gnome/nautilus/list-view"
+  "gnome/nautilus/preferences"
+  "gnome/shell/keybindings"
+  "gnome/terminal"
+)
+
+for config in "${configs[@]}"; do
+  mkdir -p "$(dirname "$config")"
+  dconf dump "/org/$config/" > "$config.ini"
+done
