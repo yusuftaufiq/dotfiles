d4337a847c7cb487eda486836e09257a0cc1501e
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-04-19 21:38:13 +0700

    feat(gnome): add a new extension

--- dconf-save.sh
+++ dconf-save.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 configs=(
+  "gnome/shell/extensions/executor"
   "gnome/shell/extensions/aylurs-widgets" 
   "gnome/shell/extensions/rounded-window-corners"
   "gnome/shell/extensions/unite"
   "gnome/shell/extensions/dash-to-dock-pop"
   "gnome/shell/extensions/pop-shell"
   "gnome/shell/extensions/pop-cosmic"
-  "gnome/shell/extensions/com/github/hermes83/compiz-windows-effect"
   "gnome/shell/extensions/ncom/github/hermes83/compiz-alike-magic-lamp-effect"
   "gnome/shell/extensions/user-theme"
   "gnome/desktop/interface"
