2c33675d533ca430824e57abccde680f0b79229c
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-04-25 20:31:08 +0700

    feat(delta): use delta as git diff tool

--- run_once_before_00-install-packages.sh
+++ run_once_before_00-install-packages.sh
@@ -20,3 +20,7 @@ cd ~
 wget https://github.com/lsd-rs/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.deb
 sudo dpkg -i lsd_0.23.1_amd64.deb
 rm lsd_0.23.1_amd64.deb
+
+wget https://github.com/dandavison/delta/releases/download/0.15.1/git-delta_0.15.1_amd64.deb
+sudo dpkg -i git-delta_0.15.1_amd64.deb
+rm git-delta_0.15.1_amd64.deb
