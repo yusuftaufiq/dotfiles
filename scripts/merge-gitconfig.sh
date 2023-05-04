960149bd3f221b56d37b51456969a2c1251fbad1
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-05-04 23:26:22 +0700

    refactor(gitconfig): use git config inside shell script to merge configurations

---
+++ scripts/merge-gitconfig.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+chezmoiSourceDir=$(chezmoi source-path)
+tempDir="$chezmoiSourceDir/temp"
+tempGitconfigFile="$tempDir/.gitconfig"
+baseGitconfigFile="$chezmoiSourceDir/.chezmoitemplates/.gitconfig"
+
+readarray -t configs < <(git config --file .chezmoitemplates/.gitconfig --list)
+
+mkdir -p "$tempDir"
+cp <(echo "$1") "$tempGitconfigFile"
+
+for config in "${configs[@]}"; do
+  key="$(echo "$config" | cut -d '=' -f 1)"
+  value="$(echo "$config" | cut -d '=' -f 2-)"
+
+  git config --file "$tempGitconfigFile" "$key" "$value"
+done
+
+cat $tempGitconfigFile
