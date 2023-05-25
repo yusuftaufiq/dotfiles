008f68053e483e1d91c075b8614d1d9555ea09a1
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-05-25 23:30:09 +0700

    fix(scripts): change commands order

--- .chezmoiscripts/run_once_before_03-install-spotify-with-spicetify.sh
+++ .chezmoiscripts/run_once_before_03-install-spotify-with-spicetify.sh
@@ -4,7 +4,8 @@ if [ -x "$(which apt)" ]; then
   curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
   echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
-  sudo apt update && sudo apt install spotify-client
+  sudo apt update
+  sudo apt install spotify-client
   sudo chmod a+wr /usr/share/spotify
   sudo chmod a+wr /usr/share/spotify/Apps -R
 
